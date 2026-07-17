067de4a159614620937743e37b1c5c80d4aab08c
Lucas Simão <novellysimaolucas@gmail.com>
2026-07-17 13:36:55 -0300

    feat: add application icons and update site favicon in layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,11 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Painel Administrativo - Infodive",
   description: "",
+  icons: {
+    icon: "/icon.svg",
+    shortcut: "/icon.svg",
+    apple: "/icon.svg",
+  },
 };
 
 export default function RootLayout({
