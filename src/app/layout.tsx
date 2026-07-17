0d4f67b07b0a571f19b437bea09157b5c5e5ddae
Lucas Simão <novellysimaolucas@gmail.com>
2026-07-17 13:51:17 -0300

    refactor: replace favicon and app icon assets and update layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,9 +11,9 @@ export const metadata: Metadata = {
   title: "Painel Administrativo - Infodive",
   description: "",
   icons: {
-    icon: "/icon.svg",
-    shortcut: "/icon.svg",
-    apple: "/icon.svg",
+    icon: "/icon.png",
+    shortcut: "/icon.png",
+    apple: "/icon.png",
   },
 };
 
